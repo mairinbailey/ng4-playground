151f0e92061fedfcae343ee530d2145009edbd44
Mairin Bailey <mairin.bailey@gmail.com>
2017-05-12 12:30:39 -0600

    add event listener to button

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,12 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!'
+
   constructor() {
     setTimeout(()=>{
       this.allowNewServer = true;
     }, 1000)
   }
   ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created!'
   }
 
 }
