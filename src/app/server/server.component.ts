dbcb29d8d18512bd300e05fd44284d69ddc518ec
Mairin Bailey <mairin.bailey@gmail.com>
2017-05-16 12:24:07 -0600

    add dynamic style with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,6 +10,15 @@ export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline'
 
+//built in method that each typescript class has which is called once this component is created
+  constructor(){
+    this.serverStatus = Math.random() > .5 ? 'online' : 'offline'
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red'
+  }
+  
   getServerStatus(){
     return this.serverStatus
   }
